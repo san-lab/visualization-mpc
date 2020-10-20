9991c439ee15edbb03edd3241a2d7dba147fa998
pegartillo95 <pegarc03@ucm.es>
2020-10-20 15:35:19 +0200

    Added variables for each of the nodes

--- vis-pricing.js
+++ vis-pricing.js
@@ -120,9 +120,9 @@ var nodes = new vis.DataSet([
       code: "\uf00c",
       size: 50,
       color: "green",
-    }, borderWidth: 2, x: 50, y: 100
+    },x: 50, y: 100
   },
-  { id: 6, label: "Audit\nlogs", shape: "square", color: "#C2FABC", x: 300, y: 250 },
+  { id: 6, label: "Audit\nlogs", shape: "square", color: "#90EE90", x: 300, y: 250 },
   {
     id: 7, shape: 'icon',
     icon: {
@@ -130,7 +130,7 @@ var nodes = new vis.DataSet([
       code: "\uf084",
       size: 30,
       color: "#f0a30a",
-    }, borderWidth: 2, x: 500, y: 80
+    }, x: 500, y: 80
   },
   {
     id: 8, shape: 'icon',
@@ -139,9 +139,19 @@ var nodes = new vis.DataSet([
       code: "\uf084",
       size: 30,
       color: "#f0a30a",
-    }, borderWidth: 2, x: 100, y: 80
+    }, x: 100, y: 80
   },
 ]);
+
+var load_weights_node = { id: 2, label: "Load\nWeights", shape: "box", color: "#FFFF00", size: 15, x: 450, y: 100 }
+var right_tick_node = { id: 3, shape: 'icon', icon: { face: "'FontAwesome'", code: "\uf00c", size: 50, color: "green", }, borderWidth: 2, x: 550, y: 100 }
+var right_cross_node = { id: 3, shape: 'icon', icon: { face: "'FontAwesome'", code: "\uf00d", size: 50, color: "red", }, borderWidth: 2, x: 550, y: 100 }
+var left_tick_node = { id: 5, shape: 'icon', icon: { face: "'FontAwesome'", code: "\uf00c", size: 50, color: "green", }, borderWidth: 2, x: 50, y: 100 }
+var left_cross_node = { id: 5, shape: 'icon', icon: { face: "'FontAwesome'", code: "\uf00d", size: 50, color: "red", }, borderWidth: 2, x: 50, y: 100 }
+var ask_pricing_node = { id: 4, label: "Ask for\npricing", shape: "box", size: 15, color: "#FFFF00", x: 150, y: 100 }
+var audit_log_node = { id: 6, label: "Audit\nlogs", shape: "square", color: "#90EE90", x: 300, y: 250 }
+var right_key_node = { id: 7, shape: 'icon', icon: { face: "'FontAwesome'", code: "\uf084", size: 30, color: "#f0a30a", }, x: 500, y: 80}
+var left_key_node = { id: 8, shape: 'icon', icon: { face: "'FontAwesome'", code: "\uf084", size: 30, color: "#f0a30a", }, x: 100, y: 80 }
 
 // create an array with edges
 var edges = new vis.DataSet([
@@ -155,8 +165,6 @@ var edges = new vis.DataSet([
   },
   { from: 2, to: 3, color: "rgb(20,24,200)" },
   { from: 4, to: 5, color: "rgb(20,24,200)" },
-  //{ from: 7, to: 2, color: "rgb(20,24,200)", arrows: "to" },
-  //{ from: 8, to: 4, color: "rgb(20,24,200)", arrows: "to" },
 ]);
 
 // create a network
