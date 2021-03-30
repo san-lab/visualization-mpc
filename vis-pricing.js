6719dcbc2b3e3878f730a719b60443aae3eda2dd
pegartillo95 <pegarc03@ucm.es>
2021-03-30 16:21:19 +0200

    Visulization for mpc

--- vis-pricing.js
+++ vis-pricing.js
@@ -1,227 +1,41 @@
-fetch('https://researchbox1.uksouth.cloudapp.azure.com/pricing/json?ID=443322')
-  .then(response => response.json())
-  .then(state => {
-
-    var starting_ask_pricing_node = 500;
-    var starting_left_doc_node = 600;
-    var starting_tick_node = 700;
-    var start_open_envelope = 900;
-    var start_close_envelope = 1000;
-
+// fetch('http://localhost:8100')
+//   .then(response => response.json())
+//   .then(state => {
     //////////////NODES//////////////
 
-    var digital_embassy_blue =  [{ id: 29, shape: "square", color: "white", size: 1, x: 580, y: 100 },{ id: 9, label: "Pricing agent", shape: "square", color: "white", size: 1, x: 300, y: 18 },{ id: 1, label: "", shape: "square", color: "#97C2FC", size: 80, x: 300, y: 100 }]
-    var digital_embassy_green = (maxCalls, remainingCalls) => {return  [{ id: 29, shape: "square", color: "white", size: 1, x: 580, y: 25 + (maxCalls-1)*25 },{ id: 10, label: remainingCalls.toString(10), shape: "box", color: "#28B463", font: {color:(remainingCalls>0) ? 'white' : 'red'}, size: 10, x: 300, y: 25 + (maxCalls-1)*25 },{ id: 9, label: "Pricing agent", shape: "square", color: "white", size: 1, x: 300, y: 25 + (maxCalls-1)*25 - 82 },{ id: 1, label: "", shape: "square", color: "#28B463", size: 80, x: 300, y: 25 + (maxCalls-1)*25 }]}
-    var load_weights_node = (maxCalls, data) => { return { id: 2, label: "Load\nWeights", shape: "box", color: "#FFFF00", size: 15, x: 445, y: 25 + (maxCalls-1)*25, title: data } }
-    var right_tick_node = (maxCalls) => { return { id: 3, shape: 'icon', icon: { face: "'FontAwesome'", code: "\uf00c", size: 20, color: "green", }, borderWidth: 2, x: 580, y: 25 + (maxCalls-1)*25 - 20 }}
-    var right_cross_node = (maxCalls) => { return { id: 3, shape: 'icon', icon: { face: "'FontAwesome'", code: "\uf00d", size: 20, color: "red", }, borderWidth: 2, x: 580, y: 25 + (maxCalls-1)*25 - 20 }}
-    var right_document_node = (maxCalls) => { return { id: 11, shape: 'icon', icon: { face: "'FontAwesome'", code: "\uf15c", size: 40, color: "black", }, borderWidth: 2, x: 550, y: 25 + (maxCalls-1)*25 }}
-    var left_tick_node = (numUser) => {return { id: starting_tick_node+numUser, shape: 'icon', icon: { face: "'FontAwesome'", code: "\uf00c", size: 20, color: "green", }, borderWidth: 2, x: 20, y: 5 + (50*numUser) }}
-    var left_cross_node = (numUser) => {return { id: starting_tick_node+numUser, shape: 'icon', icon: { face: "'FontAwesome'", code: "\uf00d", size: 20, color: "red", }, borderWidth: 2, x: 20, y: 5 + (50*numUser) }}
-    var left_document_node = (numUser) => {return { id: starting_left_doc_node+numUser, shape: 'icon', icon: { face: "'FontAwesome'", code: "\uf15c", size: 40, color: "black", }, borderWidth: 2, x: 50, y: 25 + (50*numUser) }}
-    var ask_pricing_node = (numUser) => { return { id: starting_ask_pricing_node+numUser, label: "Ask for\npricing", shape: "box", size: 15, color: "#FFFF00", x: 155, y: 25 + (50*numUser)}} // 16
-    var audit_log_node = { id: 6, label: "Audit\nlogs", shape: "square", color: "#90EE90", x: 300, y: 250 }
-     
-    //WIP
-    var model_open_envelope = (maxCalls) => { return{ id: 99, shape: 'icon', icon: { face: "'FontAwesome'", code: "\uf2b6", size: 30, color: "#f0a30a"}, x: 330, y: 25 + ((maxCalls-1)*25)}}
-    var model_close_envelope = (maxCalls) => { return { id: 100, shape: 'icon', icon: { face: "'FontAwesome'", code: "\uf0e0", size: 30, color: "#f0a30a"}, x: 390, y: 25 + ((maxCalls-1)*25) }}
-    var starting_open_envelope = (numUser, maxCalls) => {return { id: start_open_envelope+numUser, shape: 'icon', icon: { face: "'FontAwesome'", code: "\uf2b6", size: 30, color: "#f0a30a"}, x: 270, y: 25 + ((maxCalls-1)*25)} }
-    var starting_close_envelope = (numUser) => { return { id: start_close_envelope+numUser, shape: 'icon', icon: { face: "'FontAwesome'", code: "\uf0e0", size: 30, color: "#f0a30a"}, x: 200, y: (-45) + (50*(numUser+1)) }}
-
+    var node_off = (x_pos,y_pos,id_num, title_hover) => { return { id: id_num, shape: 'icon', icon: { face: "'FontAwesome'", code: "\uf1b2", size: 40, color: "black", }, borderWidth: 2, x: x_pos, y: y_pos, title:  title_hover}}
+    var node_on = (x_pos,y_pos,id_num, title_hover) => { return { id: id_num, shape: 'icon', icon: { face: "'FontAwesome'", code: "\uf1b2", size: 40, color: "green", }, borderWidth: 2, x: x_pos, y: y_pos, title:  title_hover}}
+    
     var nodes = new vis.DataSet([]);
 
     ///////////////EDGES/////////////
 
-    var enclave_askPricing_edge = (num) => {return { from: 1, to: starting_ask_pricing_node + num, color: "rgb(20,24,200)", arrows: "from" }}
-    var askPricing_document_edge = (num) => {return { from: starting_ask_pricing_node + num, to: starting_left_doc_node + num, color: "rgb(20,24,200)" }}
-
+    var between_nodes = (from_node,to_node) => {return { from: from_node, to: to_node, color: "rgb(20,24,200)", arrows: "from, to" }}
+    
     // create an array with edges
-    var edges = new vis.DataSet([
-      { from: 1, to: 2, color: "rgb(20,24,200)", arrows: "from" },
-      {
-        from: 1,
-        to: 6,
-        color: "rgb(20,24,200)",
-        arrows: "to",
-      },
-      { from: 2, to: 11, color: "rgb(20,24,200)" },
-    ]);
+    var edges = new vis.DataSet([]);
 
-    // get original positions
-    var open_envelope_start_x = 270
-    var open_envelope_start_y = 0
-    var close_envelope_start_x = 200
-    var close_envelope_start_y = 5
-
-    var left_envelope_start_x = 330
-    var left_envelope_start_y = 0
-    var left_envelope_end_x = 390
-    var left_envelope_end_y = 0
-
-    // animation properties
-    var k = 0, lambda = 0, tick = 20, totalTime = 1000;
-
-    // nr of steps, given tick time and total animation time
-    var nrOfSteps = Math.floor( totalTime / tick);
-    var timers = []
-
-    var timer_generic = (numNode, offsetTimer, offsetNode,x_start,y_start,x_target,y_target) => {
-      var data = {
-              nodes: nodes,
-              edges: edges,
-            };
-      network = new vis.Network(container, data, options);
-
-      let numIter = 0;
-      timers[numNode+offsetTimer] = setInterval(function(){
-      
-        // iteration counter
-        numIter++;
-
-        // lambda (for convex combination)
-        var l = numIter / nrOfSteps;
-
-        // compute the convex combination of x_start and x_target to find intermediate x and move node to it, same for y
-        var xt = x_start * (1 - l) + x_target * l;
-        var yt = y_start * (1 - l) + y_target * l;
-        
-        // move node
-        network.moveNode(offsetNode+numNode,xt,yt);
-      
-        // stop if we have reached nr of steps
-        if(numIter == nrOfSteps){
-          clearInterval(timers[numNode+offsetTimer])
-        } 
-      },tick);
-    }
-
-    var updateNodesAndEdges = (edges, nodes,state, prevState) => {
-      //If not undefined we will have the Central node in blue unless it is already initialized
-      let newPrevState = prevState
-      if (state != undefined) {
-        if (state["Initialized"] == true) {
-          if(prevState["init"] != true){
-            if(nodes.get(1) != null){
-              nodes.remove([1,9,29])
-            }
-            newPrevState["init"] = true;
-            nodes.add(digital_embassy_green(state["InvocationLimit"], state["InvocationLimit"]-state["InvocationCount"]));
-            nodes.add(load_weights_node(state["InvocationLimit"],state["ModelIntput"]["DataPlain"]));
-            nodes.update({id: 2, title: state["ModelIntput"]["DataEncrypted"]});
-            nodes.add(model_open_envelope(state["InvocationLimit"]));
-            left_envelope_start_y = nodes.get(99).y
-            left_envelope_end_y = left_envelope_start_y
-            timer_generic(0,200,99,left_envelope_start_x,left_envelope_start_y,left_envelope_end_x,left_envelope_end_y);
-            setTimeout(function() {
-              nodes.remove(99);
-              nodes.add(model_close_envelope(state["InvocationLimit"]));
-              timer_generic(0,201,100,left_envelope_end_x,left_envelope_end_y,left_envelope_start_x,left_envelope_start_y);
-            },3000)
-            if (state["ModelIntput"]["SignatureOK"] == true) {
-              setTimeout(function() {
-                nodes.add(right_document_node(state["InvocationLimit"]));
-                nodes.add(right_tick_node(state["InvocationLimit"]));
-                if(nodes.get(starting_ask_pricing_node) == null){
-                  for(let i = 0; i < state["InvocationLimit"]; i++){
-                    nodes.add(ask_pricing_node(i));
-                    edges.add(enclave_askPricing_edge(i));
-                    edges.add(askPricing_document_edge(i));
-                  }  
-                }
-                nodes.remove(100);
-                var data = {
-                  nodes: nodes,
-                  edges: edges,
-                };
-                network = new vis.Network(container, data, options);
-              },5500)
-            }
-            else if (state["ModelIntput"]["SignatureOK"] == false) {
-              setTimeout(function() {
-                nodes.add(right_document_node(state["InvocationLimit"]));
-                nodes.add(right_cross_node(state["InvocationLimit"]));
-                nodes.remove(100);
-                var data = {
-                  nodes: nodes,
-                  edges: edges,
-                };
-                network = new vis.Network(container, data, options);
-              },5500)
-            }
-          }
-        }
-        else {
-          if(prevState["init"] == undefined){
-            newPrevState["init"] = false;
-            nodes.add(digital_embassy_blue);
-          }
-        }
-        if (state["InvocationInputs"] != null){
-          if(state["InvocationInputs"].length > prevState["numPricing"]){
-            let remaining_Calls = state["InvocationLimit"]-state["InvocationCount"]
-            if(nodes.get(starting_ask_pricing_node) == null){
-              for(let i = 0; i < state["InvocationLimit"]; i++){
-                nodes.add(ask_pricing_node(i));
-                edges.add(enclave_askPricing_edge(i));
-                edges.add(askPricing_document_edge(i));
-              }
-            }
-            for(let i = prevState["numPricing"]; i < state["InvocationInputs"].length; i++){
-              if(i >= state["InvocationLimit"]){
-                nodes.add(ask_pricing_node(i));
-                edges.add(enclave_askPricing_edge(i));
-                edges.add(askPricing_document_edge(i));
-              }
-              nodes.update({id: starting_ask_pricing_node+i, title: state["InvocationInputs"][i]["DataEncrypted"]});
-              nodes.add(starting_open_envelope(i,state["InvocationLimit"]));
-              if(i == 0){
-                open_envelope_start_y = nodes.get(900).y
-              }
-              timer_generic(i,0,900,open_envelope_start_x,open_envelope_start_y, close_envelope_start_x, close_envelope_start_y + 50*(i));
-              setTimeout(function() {
-                nodes.remove(900+i);
-                nodes.add(starting_close_envelope(i));
-                timer_generic(i,100,1000,close_envelope_start_x,close_envelope_start_y + 50*(i),open_envelope_start_x,open_envelope_start_y);
-              },3000)
-              if (state["InvocationInputs"][i]["SignatureOK"] == true && state["InvocationInputs"][i]["ParsingOK"]==true) {
-                setTimeout(function() {
-                  nodes.add(left_document_node(i));
-                  nodes.add(left_tick_node(i));
-                  nodes.update({ id: 10, color: (remaining_Calls>0) ? "#28B463" : "#A4A4A4", label: remaining_Calls.toString(10), font: {color:(remaining_Calls>0) ? 'white' : 'red'}});
-                  nodes.update({ id: 1, color: (remaining_Calls>0) ? "#28B463" : "#A4A4A4"});
-                  nodes.update({ id: 9, label: (remaining_Calls>0) ? "Pricing agent" : "Pricing agent\n (exhausted)"})
-                  nodes.remove(1000+i);
-                  var data = {
-                    nodes: nodes,
-                    edges: edges,
-                  };
-                  network = new vis.Network(container, data, options);
-                },5500)
-              }
-              else if (state["InvocationInputs"][i]["SignatureOK"] == false || state["InvocationInputs"][i]["ParsingOK"]==false) {
-                setTimeout(function() {
-                  nodes.add(left_document_node(i));
-                  nodes.add(left_cross_node(i));
-                  if(state["InvocationInputs"][i]["SignatureOK"] == false){
-                    nodes.update({ id: starting_tick_node+i, title: "Error on signature checking"})
-                  }
-                  else {
-                    nodes.update({ id: starting_tick_node+i, title: JSON.stringify(state["InvocationInputs"][i]["Error"])})
-                  }
-                  nodes.remove(1000+i);
-                  var data = {
-                    nodes: nodes,
-                    edges: edges,
-                  };
-                  network = new vis.Network(container, data, options);
-                },5500)
-              }
-            }
-            newPrevState["numPricing"] = state["InvocationInputs"].length;
+    var updateNodesAndEdges = (edges, nodes,state, numNodesInserted) => {
+      //State machine
+      numNodes = state["Nodes"].length + 1
+      titles = []
+      titles.push(buildTitle(state["ThisName"], state["ThisId"],state["ThisAddress"]))
+      for(let i = numNodesInserted; i < numNodes-1; i++){
+        titles.push(buildTitle(state["Nodes"][i]["Name"], state["Nodes"][i]["ID"], state["Nodes"][i]["Address"]))
+      } 
+      for(let i = numNodesInserted; i < numNodes; i++){
+        nodes.add(node_on(0, 0,i, titles[i]));
+        if (i > 0) {
+          for(let j = 0; j < i; j++){
+            edges.add(between_nodes(j,i));
           }
         }
       }
-      return newPrevState;
+      return numNodes             
+    }
+
+    var buildTitle = (name, ID, address) => {
+      return "Name: " + name + "\n" + "ID: " + ID + "\n" + "Address: " + address
     }
 
     //updateNodes(state);
@@ -239,35 +53,132 @@ fetch('https://researchbox1.uksouth.cloudapp.azure.com/pricing/json?ID=443322')
       edges: {
         smooth: false
       },
-      physics: false,
-      interaction: {
-        dragNodes: false,// do not allow dragging nodes
-        zoomView: false, // do not allow zooming
-        dragView: false  // do not allow dragging
-      }
+       physics: true,
+       interaction: {
+         dragNodes: false,// do not allow dragging nodes
+         zoomView: false, // do not allow zooming
+         dragView: false  // do not allow dragging
+       }
     };
     var network = new vis.Network(container, data, options);
-    let prevState = {
-      "init": undefined,
-      "numPricing": 0,
-    }
+    var numNodesInserted = 0;
 
     function timeout() {
       setTimeout(function () {
-        fetch('https://researchbox1.uksouth.cloudapp.azure.com/pricing/json?ID=443322')
-          .then(response => response.json())
-          .then(state => {
-            prevState = updateNodesAndEdges(edges, nodes, state,prevState);
+        // fetch('http://localhost:8100')
+        //   .then(response => response.json())
+        //   .then(state => {
+          state = {
+  "ThisName": "Alice",
+  "ThisId": "3089053916941511196",
+  "ThisAddress": "172.17.0.2",
+  "ThisPassword": "",
+  "ThisEvaluationPoint": {
+   "V": 1,
+   "M": 65000549695646603732796438742359905742570406053903786389881062969044166799969,
+   "BO": false
+  },
+  "ThisSecretValue": null,
+  "ThisPublicKey": null,
+  "DisableBroadcast": false,
+  "PendingJobs": {
+   "ID0f6017556475602505628": {
+    "JobID": "ID0f6017556475602505628",
+    "AgentID": "6017556475602505628",
+    "Type": "TestJob",
+    "Finished": false,
+    "Error": "",
+    "Payload": "Are you there?"
+   },
+   "ID1f3089053916941511196": {
+    "JobID": "ID1f3089053916941511196",
+    "AgentID": "3089053916941511196",
+    "Type": "MPSignature",
+    "Finished": false,
+    "Error": "",
+    "Payload": "02000000000000000000000000000000000000000000000000000000000000000212dc2ddad8ed72726c8e7d4daaeaba8480984f3efbfae14dd71f26182498ca154d93a90afdebeb0a815a3f258a9c866f36668767525ba7348f18db1c5f1157142439646639643439342d376333632d313165622d623865362d62383662323334383934623848656c6c6f20426f62"
+   }
+  },
+  "DoneJobs": {
+   "ID0f3089053916941511196": {
+    "JobID": "ID0f3089053916941511196",
+    "AgentID": "3089053916941511196",
+    "Type": "TestJob",
+    "Finished": true,
+    "Error": "",
+    "Payload": "Are you there?"
+   }
+  },
+  "JobBroadcast": {},
+  "KnownScalarShares": {
+   "9df9d494-7c3c-11eb-b8e6-b86b234894b8": [
+    {
+     "E": {
+      "V": 2,
+      "M": 65000549695646603732796438742359905742570406053903786389881062969044166799969,
+      "BO": false
+     },
+     "V": {
+      "V": 18267298965087384827294908530404108237589431360719280554881346428202447285138,
+      "M": 65000549695646603732796438742359905742570406053903786389881062969044166799969,
+      "BO": false
+     },
+     "T": 2,
+     "SuiteID": "9df9d494-7c3c-11eb-b8e6-b86b234894b8"
+    }
+   ]
+  },
+  "Nodes": [
+   {
+    "Name": "Bob",
+    "ID": "6017556475602505628",
+    "Address": "172.17.0.3",
+    "LastSeen": "2021-03-30T09:46:14.9856168Z",
+    "PendingJobs": [
+     "ID1f3089053916941511196",
+     "ID0f6017556475602505628"
+    ],
+    "DoneJobs": [
+     "ID0f3089053916941511196"
+    ]
+   },
+   {
+    "Name": "Carol",
+    "ID": "3895312406672063063",
+    "Address": "172.17.0.4",
+    "LastSeen": "2021-03-30T09:46:15.2296065Z",
+    "PendingJobs": [
+     "ID0f3089053916941511196",
+     "ID0f6017556475602505628",
+     "ID1f3089053916941511196"
+    ],
+    "DoneJobs": []
+   },
+   {
+    "Name": "Denver",
+    "ID": "3877312406672063063",
+    "Address": "172.17.0.5",
+    "LastSeen": "2021-03-30T09:46:15.2296065Z",
+    "PendingJobs": [
+     "ID0f3089053916941511196",
+     "ID0f6017556475602505628",
+     "ID1f3089053916941511196"
+    ],
+    "DoneJobs": []
+   }
+  ]
+ }
+            numNodesInserted = updateNodesAndEdges(edges, nodes, state, numNodesInserted);
             var data = {
               nodes: nodes,
               edges: edges,
             };
             network = new vis.Network(container, data, options);
-          })
+          // })
         timeout();
       }, 7000);
     };
 
     timeout()
-  });
+  // });
 
